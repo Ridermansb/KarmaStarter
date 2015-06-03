d89a55ce5992c8c76b957cc6a1aa7024c3a76c28
ridermansb <ridermansb@gmail.com>
2015-06-03 12:51:08 -0300

    Initial Commit

---
+++ specs/helloKarma.js
@@ -0,0 +1,9 @@
+describe("hello karma", function() {
+    it("first testing is runner \o/", function () {
+        expect(true).toBe(true);
+    });
+
+    it("one plus one should be two", function() {
+        expect(1 + 1).toBe(2);
+    });
+});
